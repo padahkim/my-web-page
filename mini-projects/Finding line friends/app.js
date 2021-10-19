3996366a3bc94b4b4c97c895028bcbc663333749
padah kim <padahkim@gmail.com>
2021-10-19 23:56:43 +0900

    update makeCard function

--- mini-projects/Finding line friends/app.js
+++ mini-projects/Finding line friends/app.js
@@ -4,6 +4,9 @@ const startButton = startPage.querySelector(".start__btn");
 const startScreenImg = startPage.querySelector(".start-screen__img");
 const gamePage = document.querySelector(".game-page")
 const gameContentPage = gamePage.querySelector(".game-content");
+const cardCover = gamePage.querySelector(".cover");
+const cardBackside = gamePage.querySelector(".backside");
+const cardCoverNumber = gamePage.querySelectorAll('[count]');
 
 const HIDDEN = "hidden";
 
@@ -15,27 +18,41 @@ function startGame() {
   makeCardCover();
 }
 
-function makeCardCover() {
-  let k = 0;
-  let card = document.createElement('div');
-  card.className = "card";
-  card.innerHTML = 
-  `<div class="item-list">
-    <img data-count=` + k + `src="images/card-cover.jpg"/>
-  </div>`
- gameContentPage.append(card)
 
-  for (let i = 0; i < 16; i++) {
-  
+
+function makeCardInside() {
+  for(let j = 0; j < 2; j++) {
+    for (let i = 0; i < 8; i++) {
+      let card = document.createElement('div');
+      card.className = "item-list";
+      card.innerHTML = 
+      `<img data-count="${i}" src="images/${i+1}.jpg"/>
+      </div>`
+      cardBackside.append(card);
+    }
   }
+}
+
+function makeCardCover() {
+  for (let i = 0; i < 16; i++) {
+    let card = document.createElement('div');
+    card.className = "item-list";
+    card.innerHTML = 
+    `<img data-count="${i}" src="images/card-cover.jpg"/>
+    </div>`
+    cardCover.append(card);
+  }
+  makeCardInside();
 }
 
 
 
+
 function clickCardCover(event) {
-  if (event.target.dataset.columns) {
-    console.log(event.target.dataset.count)
-  }
+  event.target.classList.add(HIDDEN);
+  console.log(event.target);
+  console.log(event.curretTarget);
+  //event.target.dataset.count.
 }
 
 startButton.addEventListener("click", startGame);
